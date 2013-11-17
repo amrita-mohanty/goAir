408b0d3017a39bc2a560b7260f5abd77d72412bc
Amrita Mohanty <amritamohanty2004@gmail.com>
2013-11-17 00:15:26 -0800

    Changed the column names to camel case for the DB Tables.

--- db/airlineMgmt_db.sql
+++ db/airlineMgmt_db.sql
@@ -8,76 +8,76 @@ DROP TABLE IF EXISTS `employee`;
 DROP TABLE IF EXISTS `flight`;
 
 CREATE TABLE `customer` (
-  `customerid` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(40) NOT NULL,
+  `customerId` int(11) NOT NULL AUTO_INCREMENT,
+  `emailId` varchar(40) NOT NULL,
   `firstname` varchar(20) NOT NULL,
-  `lastname` varchar(20) NOT NULL,
+  `lastName` varchar(20) NOT NULL,
   `gender` varchar(1) NOT NULL,
-  `passportnum` varchar(20) NOT NULL,
+  `passportNum` varchar(20) NOT NULL,
   `nationality` varchar(20) NOT NULL,
   `address` varchar(50) NOT NULL,
   `city` varchar(20) NOT NULL,
   `state` varchar(20) NOT NULL,
   `zipcode` varchar(20) NOT NULL,
   `dob` varchar(20) NOT NULL,
-  PRIMARY KEY (`customerid`)
+  PRIMARY KEY (`customerId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `employee` (
-  `employeeid` int(11) NOT NULL AUTO_INCREMENT,
+  `employeeId` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(40) NOT NULL,
-  `firstname` varchar(20) NOT NULL,
-  `lastname` varchar(20) NOT NULL,
+  `firstName` varchar(20) NOT NULL,
+  `lastName` varchar(20) NOT NULL,
   `gender` varchar(1) NOT NULL,
-  `airlinename` varchar(50),
-  `jobdesc` varchar(20) NOT NULL,
+  `airlineName` varchar(50),
+  `jobDesc` varchar(20) NOT NULL,
   `position` varchar(20) NOT NULL,
-  `hiredate` varchar(20) NOT NULL,
+  `hireDate` varchar(20) NOT NULL,
   `address` varchar(50) NOT NULL,
   `city` varchar(20) NOT NULL,
   `state` varchar(20) NOT NULL,
   `zipcode` varchar(20) NOT NULL,
   `dob` varchar(20) NOT NULL,
-  PRIMARY KEY (`employeeid`)
+  PRIMARY KEY (`employeeId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- This is flight table to hold all the information related to flight
 CREATE TABLE  `flight` (
-  `flightid` int(5) NOT NULL AUTO_INCREMENT,
-  `flightname` varchar(10) NOT NULL,
-  `airlinename` varchar(50),
+  `flightId` int(5) NOT NULL AUTO_INCREMENT,
+  `flightName` varchar(10) NOT NULL,
+  `airlineName` varchar(50),
   `source` int(10)NOT NULL,
   `destination` varchar(40) NOT NULL,
-  `departuretime` varchar(40) NOT NULL,
-  `arrivaltime` varchar(40) NOT NULL,
+  `departureTime` varchar(40) NOT NULL,
+  `arrivalTime` varchar(40) NOT NULL,
   `status` varchar(10) NOT NULL,
-  `totalseats` int(20) NOT NULL,
-  `seatsreserved` int(10) NOT NULL,
-  `daysofweek` varchar(50) NOT NULL, -- Comma separated days for this flight
-  `flyingstartdate` TIMESTAMP NOT NULL,
-  `flyingenddate` TIMESTAMP NOT NULL,
-  PRIMARY KEY  (`flightid`)
+  `totalSeats` int(20) NOT NULL,
+  `seatsReserved` int(10) NOT NULL,
+  `daysOfWeek` varchar(50) NOT NULL, -- Comma separated days for this flight
+  `flyingStartDate` TIMESTAMP NOT NULL,
+  `flyingEndDate` TIMESTAMP NOT NULL,
+  PRIMARY KEY  (`flightId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- This table holds what all date a flight will fly with its crew id
 CREATE TABLE  `flightflyinginformation` (
-  `flightid` int(5) NOT NULL,
-  `dateofflying` TIMESTAMP NOT NULL,
-  `employeeid` int(11) NULL, 
-  FOREIGN KEY (flightid) REFERENCES flight(flightid),
-  FOREIGN KEY (employeeid) REFERENCES employee(employeeid)
+  `flightId` int(5) NOT NULL,
+  `dateOfFlying` TIMESTAMP NOT NULL,
+  `employeeId` int(11) NULL, 
+  FOREIGN KEY (flightId) REFERENCES flight(flightId),
+  FOREIGN KEY (employeeId) REFERENCES employee(employeeId)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- This table will hold the reservation information
 CREATE TABLE `reservation`(
 	`pnr` int(5) NOT NULL AUTO_INCREMENT,
-	`customerid` int(11) NOT NULL,
-	`flightid` int(5) NOT NULL,
-	`numberofseatsbooked` int(5) NOT NULL,
-	`creditcardnumber` int(16) NOT NULL,
-	`dateofbooking` TIMESTAMP NOT NULL,
-	`dateofflying` TIMESTAMP NOT NULL,
-	FOREIGN KEY (flightid) REFERENCES flight(flightid),
-  	FOREIGN KEY (customerid) REFERENCES customer(customerid),
+	`customerId` int(11) NOT NULL,
+	`flightId` int(5) NOT NULL,
+	`numberOfSeatsBooked` int(5) NOT NULL,
+	`creditCardNumber` int(16) NOT NULL,
+	`dateOfBooking` TIMESTAMP NOT NULL,
+	`dateOfFlying` TIMESTAMP NOT NULL,
+	FOREIGN KEY (flightId) REFERENCES flight(flightId),
+  	FOREIGN KEY (customerId) REFERENCES customer(customerId),
 	PRIMARY KEY  (`pnr`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
