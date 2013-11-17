50a13ebf49e76ee539564643d2506c5b2b54212f
Amrita Mohanty <amritamohanty2004@gmail.com>
2013-11-17 00:34:50 -0800

    Changes.

--- db/airlineMgmt_db.sql
+++ db/airlineMgmt_db.sql
@@ -8,52 +8,52 @@ DROP TABLE IF EXISTS `employee`;
 DROP TABLE IF EXISTS `flight`;
 
 CREATE TABLE `customer` (
-  `customerId` int(11) NOT NULL AUTO_INCREMENT,
-  `emailId` varchar(40) NOT NULL,
-  `firstname` varchar(20) NOT NULL,
-  `lastName` varchar(20) NOT NULL,
-  `gender` varchar(1) NOT NULL,
-  `passportNum` varchar(20) NOT NULL,
-  `nationality` varchar(20) NOT NULL,
-  `address` varchar(50) NOT NULL,
-  `city` varchar(20) NOT NULL,
-  `state` varchar(20) NOT NULL,
-  `zipcode` varchar(20) NOT NULL,
-  `dob` varchar(20) NOT NULL,
+  `customerId` INT(11) NOT NULL AUTO_INCREMENT,
+  `emailId` VARCHAR(40) NOT NULL,
+  `firstname` VARCHAR(20) NOT NULL,
+  `lastName` VARCHAR(20) NOT NULL,
+  `gender` VARCHAR(1) NOT NULL,
+  `passportNum` VARCHAR(20) NOT NULL,
+  `nationality` VARCHAR(20) NOT NULL,
+  `address` VARCHAR(50) NOT NULL,
+  `city` VARCHAR(20) NOT NULL,
+  `state` VARCHAR(20) NOT NULL,
+  `zipcode` VARCHAR(20) NOT NULL,
+  `dob` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`customerId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `employee` (
-  `employeeId` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(40) NOT NULL,
-  `firstName` varchar(20) NOT NULL,
-  `lastName` varchar(20) NOT NULL,
-  `gender` varchar(1) NOT NULL,
-  `airlineName` varchar(50),
-  `jobDesc` varchar(20) NOT NULL,
-  `position` varchar(20) NOT NULL,
-  `hireDate` varchar(20) NOT NULL,
-  `address` varchar(50) NOT NULL,
-  `city` varchar(20) NOT NULL,
-  `state` varchar(20) NOT NULL,
-  `zipcode` varchar(20) NOT NULL,
-  `dob` varchar(20) NOT NULL,
+  `employeeId` INT(11) NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(40) NOT NULL,
+  `firstName` VARCHAR(20) NOT NULL,
+  `lastName` VARCHAR(20) NOT NULL,
+  `gender` VARCHAR(1) NOT NULL,
+  `airlineName` VARCHAR(50),
+  `jobDesc` VARCHAR(20) NOT NULL,
+  `position` VARCHAR(20) NOT NULL,
+  `hireDate` VARCHAR(20) NOT NULL,
+  `address` VARCHAR(50) NOT NULL,
+  `city` VARCHAR(20) NOT NULL,
+  `state` VARCHAR(20) NOT NULL,
+  `zipcode` VARCHAR(20) NOT NULL,
+  `dob` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`employeeId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- This is flight table to hold all the information related to flight
 CREATE TABLE  `flight` (
-  `flightId` int(5) NOT NULL AUTO_INCREMENT,
-  `flightName` varchar(10) NOT NULL,
-  `airlineName` varchar(50),
-  `source` int(10)NOT NULL,
-  `destination` varchar(40) NOT NULL,
-  `departureTime` varchar(40) NOT NULL,
-  `arrivalTime` varchar(40) NOT NULL,
-  `status` varchar(10) NOT NULL,
-  `totalSeats` int(20) NOT NULL,
-  `seatsReserved` int(10) NOT NULL,
-  `daysOfWeek` varchar(50) NOT NULL, -- Comma separated days for this flight
+  `flightId` INT(5) NOT NULL AUTO_INCREMENT,
+  `flightName` VARCHAR(10) NOT NULL,
+  `airlineName` VARCHAR(50),
+  `source` INT(10)NOT NULL,
+  `destination` VARCHAR(40) NOT NULL,
+  `departureTime` TIMESTAMP NOT NULL,
+  `arrivalTime` TIMESTAMP NOT NULL,
+  `status` VARCHAR(10) NOT NULL,
+  `totalSeats` INT(20) NOT NULL,
+  `seatsReserved` INT(10) NOT NULL,
+  `daysOfWeek` VARCHAR(50) NOT NULL, -- Comma separated days for this flight
   `flyingStartDate` TIMESTAMP NOT NULL,
   `flyingEndDate` TIMESTAMP NOT NULL,
   PRIMARY KEY  (`flightId`)
@@ -61,20 +61,20 @@ CREATE TABLE  `flight` (
 
 -- This table holds what all date a flight will fly with its crew id
 CREATE TABLE  `flightflyinginformation` (
-  `flightId` int(5) NOT NULL,
+  `flightId` INT(5) NOT NULL,
   `dateOfFlying` TIMESTAMP NOT NULL,
-  `employeeId` int(11) NULL, 
+  `employeeId` INT(11) NULL, 
   FOREIGN KEY (flightId) REFERENCES flight(flightId),
   FOREIGN KEY (employeeId) REFERENCES employee(employeeId)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- This table will hold the reservation information
 CREATE TABLE `reservation`(
-	`pnr` int(5) NOT NULL AUTO_INCREMENT,
-	`customerId` int(11) NOT NULL,
-	`flightId` int(5) NOT NULL,
-	`numberOfSeatsBooked` int(5) NOT NULL,
-	`creditCardNumber` int(16) NOT NULL,
+	`pnr` INT(5) NOT NULL AUTO_INCREMENT,
+	`customerId` INT(11) NOT NULL,
+	`flightId` INT(5) NOT NULL,
+	`numberOfSeatsBooked` INT(5) NOT NULL,
+	`creditCardNumber` INT(16) NOT NULL,
 	`dateOfBooking` TIMESTAMP NOT NULL,
 	`dateOfFlying` TIMESTAMP NOT NULL,
 	FOREIGN KEY (flightId) REFERENCES flight(flightId),
