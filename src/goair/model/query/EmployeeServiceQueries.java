03e4414dd687d663c91dcb0a6ef342c2a0b32851
Richa Singh <richa.singh510@gmail.com>
2013-11-24 00:11:32 -0800

    employee services

---
+++ src/goair/model/query/EmployeeServiceQueries.java
@@ -0,0 +1,59 @@
+package goair.model.query;
+
+import goair.model.flight.Flight;
+import goair.model.query.employeeservices.EmployeeLoginQuery;
+import goair.model.query.employeeservices.ViewEmployeeFlightsQuery;
+import goair.util.DbConnection;
+import goair.util.SearchParametersForFlights;
+import goair.model.employee.Employee;
+
+import java.sql.Connection;
+
+import org.apache.log4j.Logger;
+
+public class EmployeeServiceQueries {
+public static Logger logger = Logger.getLogger(AdminServiceQueries.class);
+	
+	DbConnection dbConnection 	= null;
+	Connection connection 		= null;
+	
+	// Query classes
+	ViewEmployeeFlightsQuery flightsForEmployee = null;
+	EmployeeLoginQuery employeeLogin = null;
+	
+	public EmployeeServiceQueries(){
+		dbConnection 				= new DbConnection();
+		connection 					= dbConnection.createDbConnection();
+	}
+	
+	/*
+	 * This method Check whether a employee is valid or not.
+	 * If valid, then return the details of the employee set in the Employee bean.
+	 */
+	public Employee validateEmployeeLogin(String emailId,String password)
+	{
+		if(employeeLogin == null)
+		{
+			employeeLogin = new EmployeeLoginQuery();
+		}
+		logger.info("validateEmployeeLogin with params : "+ emailId + ", " + password);
+		return employeeLogin.validateEmployeeLogin(emailId, password, connection);
+	}
+	
+	
+	/**
+	 * This method will get the flight based on search parameters passed to it
+	 * @return Flight[] 
+	 */
+	public Flight[] viewEmployeeFlights(SearchParametersForFlights searchParameters)
+	{
+		if(flightsForEmployee == null)
+		{
+			flightsForEmployee = new ViewEmployeeFlightsQuery();
+		}
+		logger.info("view Employee Flights with employeeId : "+searchParameters.getEmployeeId());
+		return flightsForEmployee.viewEmployeeFlights(searchParameters, connection);
+	}
+	
+	
+}//class
